3397f50442a7ebe972a7c1e476c7d74dd36a5bd4
= <=>
2023-12-09 23:18:17 +0530

    ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,55 @@
+import { useState, useEffect } from "react";
+import { generate } from "random-words";
+import { numWords, time } from "./constants.js";
 function App() {
-  return <div className="App"></div>;
+  // state to set words to be typed
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    setWords(generateWords());
+  }, []);
+  // function to generate words
+  function generateWords() {
+    return new Array(numWords).fill(null).map(() => generate());
+  }
+  return (
+    <div className="App">
+      {/* TITLE */}
+      {/* <h1 className="title">
+        <span className="one">Typing</span> <span className="two">Master</span>
+      </h1> */}
+      {/* FOR ACCURACY AND WPM */}
+      <div className="section">
+        <div className="is-size-1 has-text-centered has-text-primary">
+          <h2>60</h2>
+        </div>
+      </div>
+      {/* START BUTTON */}
+      <div className="section">
+        <button className="button is-info is-fullwidth">Start</button>
+      </div>
+      {/* FOR INPUT */}
+      <div className="control is-expanded section">
+        <input type="text" className="input" placeholder="TYPE HERE..." />
+      </div>
+      <div className="section">
+        <div className="card">
+          <div className="card-content">
+            {/* WORDS DISPLAY SECTION */}
+            <div className="content">
+              {words.map((word, i) => (
+                <>
+                  <span key={i}>{word}</span>
+                  {i < words.length - 1 && (
+                    <span key={`separator-${i}`}> </span>
+                  )}
+                </>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const numWords = 150 // max number of words to show
+export const time = 60 // in seconds
