d4d631a46894fb8bd80240d03c90f866f84518aa
= <=>
2023-12-10 09:58:13 +0530

    login to home page done

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAHfL8O4HwCBIkdQrcXqMXO4wuS3rftvow",
     authDomain: "typing-master-d5e7b.firebaseapp.com",
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const RootComponent = () => {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={isAuth ? <App /> : <Navigate to="/login" />}
+          />
+          <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<RootComponent />);
